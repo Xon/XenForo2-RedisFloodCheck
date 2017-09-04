0851fcfea8467b2924c579f7b1331b4eec59b033
Xon <tazipper1@hotmail.com>
2017-09-04 16:45:31 +0800

    Update gitignore & switch from storing _data to _output in git

---
+++ upload/src/addons/SV/RedisFloodCheck/_output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\RedisFloodCheck\XF\Service
+{
+	class XFCP_FloodCheck extends \XF\Service\FloodCheck {}
+}
